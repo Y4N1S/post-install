babc74deeec394eb0b696ffef6f5aaff13fb46d8
Y4N1S <yanisboulot@gmail.com>
2021-01-13 10:38:37 +0100

    Modification et ajout du scipt bash et python

--- scriptbash.sh
+++ scriptbash.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-echo "UBUNTU POST-INSTALL SCRIPT"
+echo "UBUNTU POST-INSTALL SCRIPT" > log.txt
 
-echo "Updating APT..."
+echo "Updating APT..." >> log.txt
+# sudo = super utilisateur //   apt-get update permet de mettre a jour
+sudo apt update && sudo apt upgrade && sudo apt-get upgrade && sudo apt-get update 
 
-sudo apt-get update 
+# clear permet d'effacer les commandes dans le terminal 
 
-clear
-
-echo "Installing base packages"
-
-sudo apt-get install --yes git git-extras build-essential python3-pip htop glances
+clear      
 
 
-sudo snap install discord # sudo = superutilisateur snap commande va chercher dans le store ubuntu equivau a apt-get
+# echo est un équivalent de print en python3
+
+echo "Installing base packages" >>log.txt  
+
+sudo apt-get install --yes git git-extras build-essential python3-pip htop glances git python3.6 curl 
+
+echo "Installing Discord" >> log.txt
+
+#commande snap permet d'installer des logiciels depuis le store ubuntu
+
+sudo snap install discord 
+
+echo "Installing Vscode"   >> log.txt
+
+sudo snap install code --classic 
 
 
-sudo snap install code --classic #utre maniere vs code
+                                                ###### AUTRE MANIERE POUR VSCODE ######
 
+# wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb  
 
-# wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb   #relis au liens de dl
+# sudo dpkg -i ./vscode.deb   
 
-# sudo dpkg -i ./vscode.deb   #importe paquet
-
-# sudo apt-get install -f   #install
+# sudo apt-get install -f   
