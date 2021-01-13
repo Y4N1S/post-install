4be8bd25cbf909f44859654c0a2caf9e9a1bdc34
Y4N1S <76550760+Y4N1S@users.noreply.github.com>
2021-01-13 10:34:39 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,22 @@
-#!/bin/bash
+import os
 
-wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb
+def commandebash (commande):
+    os.system(commande)
 
-sudo dpkg -i ./vscode.deb
+commandebash("echo 'UBUNTU POST-INSTALL SCRIPT' > log.txt")
 
-sudo apt-get install -f
 
+
+commandebash("echo 'mise a jour de apt et apt-get' >> log.txt")
+
+commandebash("echo 'sudo apt update && sudo apt upgrade")
+
+commandebash("echo 'sudo apt-get update && sudo apt-get upgrade' >> log.txt")
+
+commandebash("echo'Installation de git curl et python' >> log.txt")
+
+commandebash("echo'apt-get install --yes git git-extras build-essential python3-pip htop glances'>> log.txt")
+
+commandebash("echo 'sudo snap install discord'>> log.txt ")
+
+commandebash("echo 'sudo snap install code --classic'>>log.txt")
