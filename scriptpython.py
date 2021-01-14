ec03d264afb2a49544e040db62c73eed2d99ab7b
Y4N1S <yanisboulot@gmail.com>
2021-01-14 14:22:37 +0100

    :/Users/utilisateur/AppData/Local/Programs/Python/Python39/python.exe c:/Users/utilisateur/Downloads/groupes-alleatoire.p
    q
    exit
    
    :q
    :qa

--- scriptpython.py
+++ scriptpython.py
@@ -13,9 +13,9 @@ commandebash("echo 'sudo apt update && sudo apt upgrade")
 
 commandebash("echo 'sudo apt-get update && sudo apt-get upgrade' >> log.txt")
 
-commandebash("echo'Installation de git curl et python' >> log.txt")
+commandebash("echo 'Installation de git curl et python' >> log.txt")
 
-commandebash("echo'apt-get install --yes git git-extras build-essential python3-pip htop glances'>> log.txt")
+commandebash("echo 'apt-get install --yes git git-extras build-essential python3-pip htop glances'>> log.txt")
 
 commandebash("echo 'sudo snap install discord'>> log.txt ")
 
